6020cbd7afb4cb511769a568dae2304cb3e3cf62
jurabek10 <samiev.jurabek@bk.ru>
2024-09-24 03:23:16 +0900

    feat: publish layout components

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,20 +5,18 @@ import { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <Stack>
-      <Stack flexDirection={"column"}>
-        <Stack>
-          <Stack className="container">Popular Properties</Stack>
-        </Stack>
-        <Stack>
-          <Stack className="container">Top Agents</Stack>
-        </Stack>
-        <Stack>
-          <Stack className="container">Top Properties</Stack>
-        </Stack>
-        <Stack>
-          <Stack className="container">Events</Stack>
-        </Stack>
+    <Stack className={"home-page"}>
+      <Stack>
+        <Stack className="container">Popular Properties</Stack>
+      </Stack>
+      <Stack>
+        <Stack className="container">Top Agents</Stack>
+      </Stack>
+      <Stack>
+        <Stack className="container">Top Properties</Stack>
+      </Stack>
+      <Stack>
+        <Stack className="container">Events</Stack>
       </Stack>
     </Stack>
   );
